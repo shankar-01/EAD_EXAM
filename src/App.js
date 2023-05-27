de71303f9aff2c5cf3441064e8e11a9297cf0ae6
shankar-01 <shankar.bscsf19@iba-suk.edu.pk>
2023-05-27 16:25:38 +0500

    Add App Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { PolllDisplay } from './PollDisplay';
+import { useEffect, useState } from 'react';
+import { UserParticipation } from './UserParticipation';
+import { ThankYouMessage } from './ThankYouMessage';
+import { AnotherUserParticipation } from './AnotherUserParticipation';
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(()=>{
+    setData({
+
+      "question": "What is your favorite programming language?",
+    
+      "choices": [
+    
+        { "id": 1, "label": "JavaScript", "votes": 0 },
+    
+        { "id": 2, "label": "Python", "votes": 0 },
+    
+        { "id": 3, "label": "Java", "votes": 0 },
+    
+        { "id": 4, "label": "C#", "votes": 0 }
+      ] 
+  })
+  }
+  ,[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Polling App</h1>
+    <PolllDisplay {...data}/>
+    <UserParticipation/>
+    <ThankYouMessage/>
+    <AnotherUserParticipation/>
+    </>
   );
 }
 
